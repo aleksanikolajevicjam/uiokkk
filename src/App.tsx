817ea9ea8e826591e1785d062525aae4405c9e8c
aleksanikolajevicjam <106659421+aleksanikolajevicjam@users.noreply.github.com>
2023-04-12 10:55:29 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+import { BookingForm } from "./selector"
+
+export default function App() {
+  return (
+    <div className="h-[2000px] pt-8">
+      <BookingForm />
+    </div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { render } from "react-dom"
+import "flatpickr/dist/themes/material_green.css"
+
+import App from "./App"
+
+const rootElement = document.getElementById("root")
+render(<App />, rootElement)
